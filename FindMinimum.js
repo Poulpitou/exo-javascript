a820bef09069dfb857d4432487ce6da0e59d65db
Poulpitou <berengere.hosselet@gmail.com>
2019-03-24 19:29:53 +0100

    Correction exo Minimum

--- FindMinimum.js
+++ FindMinimum.js
@@ -1,11 +1,11 @@
 function minimum(nbArray) {
     let mini = nbArray[0];
     for (let i = 1; i < nbArray.length; i++) {
-      if (mini > nbArray[i]) {
+      if (nbArray[i] < mini) {
         mini = nbArray[i];
-        return mini;
       }
     }
+    return mini;
   }
   
   console.log("Minimum", minimum([0, 1, 2, 9, 5, -6, -4, 4, -4, 3, 4, -1.5]));
